80709c3f38fc13143ba1be591ddde635219aa41c
shahin2525 <shahin451999@gmail.com>
2024-03-22 16:52:27 -0700

    balance added

--- js/deposit.js
+++ js/deposit.js
@@ -1,10 +1,19 @@
 document.getElementById("deposit-btn").addEventListener("click", function () {
   const depositInputField = document.getElementById("deposit-input");
-  const depositInputValue = depositInputField.value;
+  const depositInputValueString = depositInputField.value;
+  const newDeposit = parseFloat(depositInputValueString);
   const depositTextElement = document.getElementById("deposit-text");
-  const depositTotal = depositTextElement.innerText;
-  depositTextElement.innerText = depositInputValue;
+  const depositTotalString = depositTextElement.innerText;
+  const previousDepositTotal = parseFloat(depositTotalString);
+  const currentDepositTotal = previousDepositTotal + newDeposit;
+  depositTextElement.innerText = currentDepositTotal;
 
+  //   balance
+  const balanceElementText = document.getElementById("balance-text");
+  const balanceElementTextString = balanceElementText.innerText;
+  const previousBalance = parseFloat(balanceElementTextString);
+  const newBalanceTotal = newDeposit + previousBalance;
+  balanceElementText.innerText = newBalanceTotal;
   //
   depositInputField.value = "";
 });
